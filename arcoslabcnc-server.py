f3542c1a685c8e586e55865bf7e783bfda5170f9
Federico Ruiz Ugalde <memeruiz@gmail.com>
2017-01-10 09:04:26 -0600

    adjustment for better speed and pulse management

--- arcoslabcnc-server.py
+++ arcoslabcnc-server.py
@@ -3,8 +3,9 @@
 from gpiozero import DigitalOutputDevice as dig
 from gpiozero import DigitalOutputDevice as pwm
 from time import sleep
+import time
 from math import pi, atan, sin, cos
-
+from multiprocessing import Process, Queue
 input_port_name="/cnc/cmd:i"
 
 
@@ -96,8 +97,44 @@ class Axis:
         if speed==0.0:
           distance=0.0
           speed=0.001
-        print "Moving: ", distance/self.pitch, " revolutions"
+        #print "Moving: ", distance/self.pitch, " revolutions"
         self.stepper.move_angle(360.0*distance/self.pitch, speed/self.pitch)
+
+class PreciseSpeed:
+  def __init__(self, pin):
+    self.queue=Queue()
+    self.process=Process(target=self.loop, args=(pin, self.queue,))
+    self.process.start()
+
+  def loop(self, pin, queue):
+    print "Starting loop"
+    self.dev=pwm(pin, active_high=False)
+    steps,freq=(0,0)
+    print "First loop data: ", steps, freq
+    while True:
+      print "*******Looping!!!  Steps: ", steps, " Queue size: ", queue.qsize()
+      try:
+        while not queue.empty():
+          steps,freq=queue.get(False)
+          print "*****Loop: New steps and freq: ", steps, freq
+      except:
+        pass
+        #print "No data in queue"
+      if steps>0:
+        print "*********Loop: Step1: ", steps
+        steps-=1
+        self.dev.on()
+        sleep((1.0/freq)/2.0)
+        self.dev.off()
+        sleep((1.0/freq)/2.0)
+        print "*********Loop: Step2: ", steps
+      else:
+        #print "No steps, motor off"
+        self.dev.off()
+      sleep(0.01)
+
+  def move_steps(self, steps, freq=1.0):
+    self.queue.put((steps, freq))
 
 class Stepper:
     def __init__(self, pulse_pin, dir_pin, ena_pin, step=0.9, period=0.002):
@@ -107,6 +144,7 @@ class Stepper:
         self.ena_pin=ena_pin
         self.step=step
         self.pulse=pwm(self.pulse_pin, active_high=False)
+        #self.precisespeed=PreciseSpeed(pulse_pin)
         #self.pulse.value=0.5
         self.direction=dig(self.dir_pin, active_high=False)
         self.ena=dig(self.ena_pin, active_high=True)
@@ -114,7 +152,12 @@ class Stepper:
         self.off_time=self.on_time
         self.dir_wait=self.on_time
         self.step_rest=0.0
+        self.period=0.0
         self.disable()
+        self.time=time.time()
+        self.time_last_toggle=self.time
+        self.state=False
+        self.steps=0
 
     def enable(self):
         self._enable=True
@@ -127,7 +170,7 @@ class Stepper:
         self._enable=False
         self.ena.off()
         self.direction.off()
-        self.pulse.off()
+        #self.pulse.off()
 
     def move_angle(self, delta_angle, rps):
         #angle in degrees
@@ -148,8 +191,9 @@ class Stepper:
 
     def move_steps(self, steps, speed):
         #speed in steps per second
-        period=1.0/speed
-        print "Steps: ", steps, " Speed: ", speed, " period: ", period
+        self.period=1.0/speed
+        self.steps=abs(steps)
+        #print "Steps: ", steps, " Speed: ", speed, " period: ", self.period
         if self._enable:
             if steps<0:
             #    print "Pos dir"
@@ -157,11 +201,31 @@ class Stepper:
             else:
             #    print "Neg dir"
                 self.direction.off()
-            sleep(self.dir_wait)
+            #sleep(self.dir_wait)
             #print "Pulsing"
-            self.pulse.blink(on_time=period/2.0, off_time=period/2.0, n=abs(steps), background=True)
+            #self.pulse.blink(on_time=period/2.0, off_time=period/2.0, n=abs(steps), background=True)
             #self.toggle(n=abs(steps))
+            #self.precisespeed.move_steps(abs(steps), speed)
             #print "Finished pulsing"
+
+    def update(self):
+      #print "Updating motor"
+      if self._enable:
+        new_time=time.time()
+        if self.period==0.0:
+          self.pulse.off()
+        else:
+          if (new_time-self.time_last_toggle)>=self.period:
+            #print "*****Time to toggle! ", new_time-self.time_last_toggle
+            self.time_last_toggle+=self.period
+            if self.steps>0:
+              if self.state:
+                self.pulse.off()
+                self.state=False
+                self.steps-=1
+              else:
+                self.pulse.on()
+                self.state=True
 
     def toggle(self, n=1):
         for i in xrange(n):
@@ -191,15 +255,16 @@ if __name__=="__main__":
     input_bottle=input_port.read(False)
     if input_bottle:
       input_data=input_bottle.get(0).asString()
-      print "Input data: ", input_data
+      #print "Input data: ", input_data
       data=input_data.split(" ")
-      print "Data: ", data
+      #print "Data: ", data
       if data[0]=="speed":
-        print "Speed command: ", float(data[1]), float(data[2])
+        #print "Speed command: ", float(data[1]), float(data[2])
         axisx.move(0.01, speed=float(data[1]))
         axisy.move(0.01, speed=float(data[2]))
-    else:
-      yarp.Time.delay(0.1)
+    motx.update()
+    moty.update()
+    yarp.Time.delay(0.0001)
 
   #circle1=Circle(axisx, axisy)
   #circle1.do_circle(0.01, cut_speed=0.0005, move_speed=0.05)
