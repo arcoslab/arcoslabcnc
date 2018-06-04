5d6a65ca9ca5b94a22d81a74044dee93489623c3
Federico Ruiz Ugalde <memeruiz@gmail.com>
2018-06-04 07:04:44 -0600

    arcoslabcnc-server.py: backlash compensation

--- software/arcoslabcnc-server.py
+++ software/arcoslabcnc-server.py
@@ -87,11 +87,18 @@ def gen_arc(cur_x, cur_y, cur_z, x, y ,z, i, j, res, speed, angle_res_factor=1.0
 
 
 class Axis:
-    def __init__(self, stepper, pitch=0.005):
+    def __init__(self, stepper, pitch=0.005, backlash=0.002):
         #pitch = meters per revolution
         self.pitch=pitch
         self.stepper=stepper
+        self.backlash=backlash
         self.res=(self.stepper.step/360.)*self.pitch
+        self.last_move_right=True
+        self.changed_dir=False
+        self.cur_pos=0.0
+        self.cur_head_pos=self.cur_pos-self.backlash/2.0
+        self.last_head_pos_before_change_dir=self.cur_head_pos
+        self.stepper.move_angle2(360.0*(self.cur_pos)/self.pitch, 0.001/self.pitch)
 
     def disable(self):
         self.stepper.disable()
@@ -115,7 +122,8 @@ class Axis:
         self.stepper.move_angle(360.0*distance/self.pitch, speed/self.pitch)
 
     def move2(self, distance, speed=0.001):
-        self.move_abs2(self.cur_pos+distance, speed=speed)
+        #print "moving relative: ", distance, self.cur_head_pos+distance
+        self.move_abs2(self.cur_head_pos+distance, speed=speed)
 
     def move_abs(self, pos, speed=0.001):
         self.move(pos-self.cur_pos, speed)
@@ -126,15 +134,62 @@ class Axis:
         if speed<0.0:
           speed=-speed
         if speed==0.0:
-          speed=0.00000000001
+          speed=0.000001
+        if speed<0.000001:
+            speed=0.000001
         #print "Moving: ", distance/self.pitch, " revolutions"
-        self.stepper.move_angle2(360.0*pos/self.pitch, speed/self.pitch)
+        offset=0.0
+        if abs(pos-self.cur_head_pos)<self.res:
+            return()
+        if self.last_move_right:
+            if (pos-self.cur_head_pos)>0.0:
+                print "Still moving right"
+                offset=self.backlash/2.0
+            elif (pos-self.cur_head_pos)<0.0:
+                print "Changing direction from right to left"
+                offset=-self.backlash/2.0
+                self.last_move_right=False
+                self.changed_dir=True
+                self.last_head_pos_before_change_dir=self.cur_head_pos
+                print "Last head pos: ", self.cur_head_pos
+            else:
+                print "No movement commanded"
+                return()
+        else:
+            if (pos-self.cur_head_pos)<0.0:
+                print "Still moving left"
+                offset=-self.backlash/2.0
+            elif (pos-self.cur_head_pos)>0.0:
+                print "Changing direction from left to right"
+                offset=self.backlash/2.0
+                self.last_move_right=True
+                self.changed_dir=True
+                self.last_head_pos_before_change_dir=self.cur_head_pos
+                print "Last head pos: ", self.cur_head_pos
+            else:
+                print "No movement commanded"
+                return()
+        #print "Stepper move command pos: ", pos+offset, speed/self.pitch
+        self.stepper.move_angle2(360.0*(pos+offset)/self.pitch, speed/self.pitch)
 
     def set_to(self, pos):
         self.stepper.set_to(360.0*pos/self.pitch)
-        
+
     def update(self):
         self.cur_pos=(self.stepper.cur_angle/360.0)*self.pitch
+        if self.changed_dir: # it changed dir from last command and still hasn't overcomed backlash
+            if ((not self.last_move_right) and ((self.cur_pos-self.last_head_pos_before_change_dir)>(-self.backlash/2.0)+(2*self.pitch*self.stepper.step/360.0))) or ((self.last_move_right) and ((self.cur_pos-self.last_head_pos_before_change_dir)<(self.backlash/2.0)-(2*self.pitch*self.stepper.step/360.0))):
+                #print "Still inside backlash area", self.cur_pos-self.last_head_pos_before_change_dir, self.cur_pos, self.last_head_pos_before_change_dir
+                self.cur_head_pos=self.last_head_pos_before_change_dir
+            else:
+                print "Out of backlash area", self.cur_pos-self.last_head_pos_before_change_dir, self.cur_pos, self.last_head_pos_before_change_dir
+                self.changed_dir=False
+        if not self.changed_dir:
+            if self.last_move_right:
+                self.cur_head_pos=self.cur_pos-self.backlash/2.0
+            else:
+                self.cur_head_pos=self.cur_pos+self.backlash/2.0
+            self.last_head_pos_before_change_dir=self.cur_head_pos
 
 class PreciseSpeed:
   def __init__(self, pin):
@@ -353,9 +408,13 @@ class Stepper_sim:
                 #print "Not correction, update vis now. ", self.cur_angle
                 self.vis.move(self.cur_angle*0.0001, self.axis)
                 self.last_vis_angle=self.cur_angle
+      else:
+          #print "Motor disabled"
+          pass
 
     def isBusy2(self):
         if abs(self.ref_angle-self.cur_angle)>(self.step/2.0):
+            #print "is busy2. Ref angle: ", self.ref_angle, " cur angle: ", self.cur_angle
             return(True)
         else:
             return(False)
@@ -582,9 +641,9 @@ if __name__=="__main__":
       moty=Stepper(17, 27, 22, step=0.225)
       motz=Stepper(10, 9, 11, step=0.1125) # half the step of the others because of the belt reduction factor
   #motz=Stepper(10, 9, 11)
-  axisx=Axis(motx)
-  axisy=Axis(moty)
-  axisz=Axis(motz)
+  axisx=Axis(motx, backlash=0.0002)
+  axisy=Axis(moty, backlash=0.0002)
+  axisz=Axis(motz, backlash=0.0002)
   axisx.enable()
   axisy.enable()
   axisz.enable()
@@ -599,6 +658,25 @@ if __name__=="__main__":
             axisx.move_abs2(ax, speed=speedx)
             axisy.move_abs2(ay, speed=speedy)
             axisz.move_abs2(az, speed=speedz)
+            axisx.update()
+            axisy.update()
+            axisz.update()
+            motx.disable()
+            moty.disable()
+            motz.disable()
+            if axisx.changed_dir: # if it must correct backlash, stop other axis meanwhile
+                #print "Axis y, z waiting for x to correct backlash"
+                motx.enable()
+            if axisy.changed_dir:
+                #print "Axis x, z waiting for y to correct backlash"
+                moty.enable()
+            if axisz.changed_dir:
+                #print "Axis y, x waiting for z to correct backlash"
+                motz.enable()
+            if (not axisx.changed_dir) and (not axisy.changed_dir) and (not axisz.changed_dir):
+                motx.enable()
+                moty.enable()
+                motz.enable()
             motx.update2()
             moty.update2()
             motz.update2()
@@ -643,6 +721,9 @@ if __name__=="__main__":
                   axisx.move2(dx, speed=speedx)
                   axisy.move2(dy, speed=speedy)
                   axisz.move2(dz, speed=speedz)
+                  axisx.update()
+                  axisy.update()
+                  axisz.update()
               else:
                   print "Don't move!, already there!"
           else:
@@ -653,15 +734,18 @@ if __name__=="__main__":
               x=float(data[1])
               y=float(data[2])
               z=float(data[3])
-              d=sqrt((x-axisx.cur_pos)**2+(y-axisy.cur_pos)**2+(z-axisz.cur_pos)**2)
+              d=sqrt((x-axisx.cur_head_pos)**2+(y-axisy.cur_head_pos)**2+(z-axisz.cur_head_pos)**2)
               if d>0:
-                  speedx=float(data[4])*abs((x-axisx.cur_pos)/d)
-                  speedy=float(data[4])*abs((y-axisy.cur_pos)/d)
-                  speedz=float(data[4])*abs((z-axisz.cur_pos)/d)
+                  speedx=float(data[4])*abs((x-axisx.cur_head_pos)/d)
+                  speedy=float(data[4])*abs((y-axisy.cur_head_pos)/d)
+                  speedz=float(data[4])*abs((z-axisz.cur_head_pos)/d)
                   #print "d: ", d, speedx, speedy, speedz
                   axisx.move_abs2(x, speed=speedx)
                   axisy.move_abs2(y, speed=speedy)
                   axisz.move_abs2(z, speed=speedz)
+                  axisx.update()
+                  axisy.update()
+                  axisz.update()
               else:
                   print "Don't move!, already there!"
           else:
@@ -680,9 +764,9 @@ if __name__=="__main__":
               j_arc=float(data[5])
               speed=float(data[6])
               angle_res_factor=float(data[7])
-              d=sqrt((x-axisx.cur_pos)**2+(y-axisy.cur_pos)**2+(z-axisz.cur_pos)**2)
+              d=sqrt((x-axisx.cur_head_pos)**2+(y-axisy.cur_head_pos)**2+(z-axisz.cur_head_pos)**2)
               if d>0.0:
-                  arc_cmd_list=gen_arc(axisx.cur_pos, axisy.cur_pos, axisz.cur_pos, x, y , z, i_arc, j_arc, axisx.res, speed, angle_res_factor, cw=cw)
+                  arc_cmd_list=gen_arc(axisx.cur_head_pos, axisy.cur_head_pos, axisz.cur_head_pos, x, y , z, i_arc, j_arc, axisx.res, speed, angle_res_factor, cw=cw)
                   move_buffer+=arc_cmd_list
                   print "Buffer updated", move_buffer
                   # for ax,ay,az,speedx,speedy,speedz in arc_cmd_list:
@@ -732,15 +816,15 @@ if __name__=="__main__":
         print "Pull drill down"
         raw_input()
       elif data[0]=="cur_pos":
-        print "Current Position"
+        print "Current Position", axisx.cur_head_pos, axisy.cur_head_pos, axisz.cur_head_pos, axisx.cur_pos, axisy.cur_pos, axisz.cur_pos
         pos_bottle=pos_port.prepare()
         pos_bottle.clear()
-        pos_bottle.addDouble(axisx.cur_pos)
-        pos_bottle.addDouble(axisy.cur_pos)
-        pos_bottle.addDouble(axisz.cur_pos)
-        pos_bottle.addDouble(axisx.cur_pos/0.0254)
-        pos_bottle.addDouble(axisy.cur_pos/0.0254)
-        pos_bottle.addDouble(axisz.cur_pos/0.0254)
+        pos_bottle.addDouble(axisx.cur_head_pos)
+        pos_bottle.addDouble(axisy.cur_head_pos)
+        pos_bottle.addDouble(axisz.cur_head_pos)
+        pos_bottle.addDouble(axisx.cur_head_pos/0.0254)
+        pos_bottle.addDouble(axisy.cur_head_pos/0.0254)
+        pos_bottle.addDouble(axisz.cur_head_pos/0.0254)
         pos_port.writeStrict()
       elif data[0]=="reset_pos":
         print "Resetting position!"
@@ -765,13 +849,35 @@ if __name__=="__main__":
           #status_bottle.addInt(int(busy))
           #status_port.write()
     counter+=1
+    motx.disable()
+    moty.disable()
+    motz.disable()
+    if axisx.changed_dir: # if it must correct backlash, stop other axis meanwhile
+        #print "Axis y, z waiting for x to correct backlash"
+        motx.enable()
+    if axisy.changed_dir:
+        #print "Axis x, z waiting for y to correct backlash"
+        moty.enable()
+    if axisz.changed_dir:
+        #print "Axis y, x waiting for z to correct backlash"
+        motz.enable()
+    if (not axisx.changed_dir) and (not axisy.changed_dir) and (not axisz.changed_dir):
+        motx.enable()
+        moty.enable()
+        motz.enable()
+    #print "motx update"
     motx.update2()
+    #print "moty update"
     moty.update2()
+    #print "motz update"
     motz.update2()
+    #print "axisx update"
     axisx.update()
+    #print "axisy update"
     axisy.update()
+    #print "axisz update"
     axisz.update()
-    yarp.Time.delay(0.00001)
+    yarp.Time.delay(0.0001)
 
   #circle1=Circle(axisx, axisy)
   #circle1.do_circle(0.01, cut_speed=0.0005, move_speed=0.05)
